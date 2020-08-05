a7bde894e8530b55e9c1b3fc721d281e1bc5251d
pranjal072 <pranjaltyagi97@gmail.com>
2020-08-05 14:26:27 +0530

    Added Condition for Part time Work

--- EmpWage.java
+++ EmpWage.java
@@ -2,15 +2,21 @@ public class EmpWage
 {
    public static void main(String args[])
    {
+		int fullTime = 1, partTime = 2;
 		int workHr,wage;
 		int rateperHr = 20;
 		System.out.println("Welcome to Employee Wage Calculation !!");
-		double empCheck = Math.floor(Math.random() * 10 ) % 2;
-		if (empCheck == 1)
+		double empCheck = Math.floor(Math.random() * 10 ) % 3;
+		if (empCheck == fullTime)
 		{
-			System.out.println("Employee is Present");
+			System.out.println("Employee is Present - Full Time");
 			workHr = 8;
 		}
+		else if ( empCheck == partTime )
+      {
+         System.out.println("Employee is Present - Part Time");
+         workHr=4;
+      }
 		else
 		{
 			System.out.println("Employee is Absent");
