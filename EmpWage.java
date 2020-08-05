0ba7fd396a509b3acc1b65029660e6f7703cfe52
pranjal072 <pranjaltyagi97@gmail.com>
2020-08-05 14:06:07 +0530

    Attendance Check for Employee added

--- EmpWage.java
+++ EmpWage.java
@@ -3,5 +3,10 @@ public class EmpWage
    public static void main(String args[])
    {
 		System.out.println("Welcome to Employee Wage Calculation !!");
+		double empCheck = Math.floor(Math.random() * 10 ) % 2;
+		if (empCheck == 1)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 }
