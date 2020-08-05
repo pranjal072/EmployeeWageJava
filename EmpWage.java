1ccb53088f57e49e6a8992791cf0f388f075a78b
pranjal072 <pranjaltyagi97@gmail.com>
2020-08-05 14:17:57 +0530

    Added Daily Wage of employee to be displyed

--- EmpWage.java
+++ EmpWage.java
@@ -2,11 +2,21 @@ public class EmpWage
 {
    public static void main(String args[])
    {
+		int workHr,wage;
+		int rateperHr = 20;
 		System.out.println("Welcome to Employee Wage Calculation !!");
 		double empCheck = Math.floor(Math.random() * 10 ) % 2;
 		if (empCheck == 1)
+		{
 			System.out.println("Employee is Present");
+			workHr = 8;
+		}
 		else
+		{
 			System.out.println("Employee is Absent");
+			workHr = 0;
+		}
+		wage = workHr*rateperHr;
+		System.out.println("Wage for today is : " + wage);
 	}
 }
